902cb2cbbdc2565754df4e78bc5bfbb74a99a248
tom-sapletta-com <tom@sapletta.com>
2023-12-07 16:42:46 +0100

    test before refactoring

--- multigit.py
+++ multigit.py
@@ -148,6 +148,7 @@ def create_repo_on_github(api_token, org_name, repo_name, local_path, descriptio
         'name': repo_name,
         'description': description,
         'homepage': "http://" + repo_name + "." + domain,
+        'html_url': "http://" + repo_name + "." + domain,
         'private': False  # Set to True if you want a private repository
     }
 
@@ -175,6 +176,30 @@ def create_repo_on_github(api_token, org_name, repo_name, local_path, descriptio
 
     else:
         print('Failed to create repo:', response.content)
+
+
+# create repository on github by api call
+def update_repo_on_github(api_token, org_name, repo_name, description, domain):
+    # Endpoint to create a repo within an organization
+    url = f'https://api.github.com/repos/{org_name}/{repo_name}/pages'
+    print(url)
+    # Data for the new repo
+    data = {
+        'name': repo_name,
+        'description': description,
+        'homepage': "http://" + repo_name + "." + domain,
+        'html_url': "http://" + repo_name + "." + domain,
+        #'private': False  # Set to True if you want a private repository
+    }
+
+    # Make the request
+    response = requests.put(url, json=data, headers=getHeaders(api_token))
+
+    # Check the response from GitHub
+    if response.status_code == 201:
+        print(f'updated repo {repo_name} under organization {org_name}.')
+    else:
+        print('Failed to update repo:', response.content)
 
 
 def load_file(filename):
@@ -624,7 +649,8 @@ if __name__ == "__main__":
         branch = 'main'
         repo_name = 'identity'
         # exit()
-        homepage = get_param_from_repo(repos, 'homepage')
+        homepage=''
+        #homepage = get_param_from_repo(repos, 'homepage')
         #print(homepage)
 
         if homepage:
@@ -640,7 +666,10 @@ if __name__ == "__main__":
         description = repo_name + ', ' + homepage
 
         print(org_name, domain, homepage, description)
-        #exit()
+
+        update_repo_on_github(api_token, org_name, repo_name, description, domain)
+        exit()
+
         #change_default_branch_to_main(api_token, org_name, repo_name, description, homepage)
         set_github_pages_domain(api_token, org_name, domain)
         # clone_repos_from_org(org_name, repos, path_name, local_path)
